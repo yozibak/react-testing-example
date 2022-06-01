149152eb60bbf7f156119d54b9513019feddbc42
Katsumi Yoshida <yozibak@gmail.com>
2022-06-01 09:19:40 +0900

    starter for e2e tutorial

--- e2e/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import { IndexPage } from "./IndexPage"
-import { LoginPage } from "./LoginPage"
-import { DashboardPage } from "./DashboardPage"
-import { DetailPage } from "./DetailPage"
-export { IndexPage, LoginPage, DashboardPage, DetailPage }
